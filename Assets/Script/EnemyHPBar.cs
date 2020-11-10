e3956fbfe02d5c2d968e9a5515111fe144d2209e
M.Ryuya <56579452+MiyoshiRyuya@users.noreply.github.com>
2020-11-10 10:07:51 +0900

    敵のHPバー途中

---
+++ Assets/Script/EnemyHPBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UI使うときは忘れずに。
+using UnityEngine.UI;
+
+public class EnemyHPBar : MonoBehaviour
+{
+    //最大HPと現在のHP。
+    int maxHp = 400;
+    int currentHp;
+    //Sliderを入れる
+    public Slider slider;
+
+    void Start()
+    {
+        //Sliderを満タンにする。
+        slider.value = 1;
+        //現在のHPを最大HPと同じに。
+        currentHp = maxHp;
+        Debug.Log("Start currentHp : " + currentHp);
+    }
+
+    //ColliderオブジェクトのIsTriggerにチェック入れること。
+    private void OnTriggerEnter(Collider collider)
+    {
+        //Playerタグのオブジェクトに触れると発動
+        if (collider.gameObject.tag == "Player")
+        {
+            //ダメージは50～100の中でランダムに決める。
+            int damage = Random.Range(50, 100);
+            Debug.Log("damage : " + damage);
+
+            //現在のHPからダメージを引く
+            currentHp = currentHp - damage;
+            Debug.Log("After currentHp : " + currentHp);
+
+            //最大HPにおける現在のHPをSliderに反映。
+            //int同士の割り算は小数点以下は0になるので、
+            //(float)をつけてfloatの変数として振舞わせる。
+            slider.value = (float)currentHp / (float)maxHp; ;
+            Debug.Log("slider.value : " + slider.value);
+        }
+    }
+}
